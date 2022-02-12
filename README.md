9fbcce7c28e3a570e1a108f1ed4095ceb9aa4015
HonorKnight8 <Honor_8Virtues@outlook.com>
2022-02-12 12:07:09 +0800

    20220212_1206
    0.1.03 release

--- README.md
+++ README.md
@@ -10,8 +10,16 @@ gdflbd，generate datasets for learning big data，生成用于学习大数据�
 
 命令示例：
 
-- `java -jar gdflbd-jar-with-dependencies.jar D:\test\gdflbd\1224test\t AppStartLog tiny 2020-08-20 14`
-- `java -jar gdflbd-jar-with-dependencies.jar /root/gdflbd1224/test_tiny AppStartLog tiny 2020-08-20 8`
+- ```shell
+  # win
+  java -jar gdflbd_0.1.03-jar-with-dependencies.jar D:\test\gdflbd\0212\tiny AppStartLog tiny 2020-08-20 14
+  
+  # Linux
+  java -jar gdflbd_0.1.03-jar-with-dependencies.jar /root/test/gdflbd/0212/tiny AppStartLog tiny 2020-08-20 8
+  
+  # OrderDB 类型
+  java -jar gdflbd_0.1.03-jar-with-dependencies.jar D:\test\gdflbd\0212\orderdb_tiny OrderDB tiny 2020-09-21 8
+  ```
 
 
 
@@ -21,7 +29,7 @@ gdflbd，generate datasets for learning big data，生成用于学习大数据�
   - 目标文件夹
 - param 2：
   - 要生成的数据集类型
-  - 当前支持的数据集类型有：[AppStartLog]
+  - 当前支持的数据集类型有：[AppStartLog, OrderDB]
 - param 3：
   - 要生成的数据集规模
   - 当前支持的数据集规模有：[Tiny, Small, Medium, Large, Huge]
@@ -36,11 +44,53 @@ gdflbd，generate datasets for learning big data，生成用于学习大数据�
 
 ## 数据集类型说明：
 
-- AppStartLog ，模拟 APP 启动日志，样本如下：
+- AppStartLog 类型
 
+  -  模拟 APP 启动日志，样本如下：
+  
   - ```log
     2021-12-25 00:08:28.000 [logToFile] INFO  monster.helloworld.gdflbd.logger.AppStartLogger - {"app_active":{"name":"app_active","json":{"entry":"1","action":"0","error_code":"0"},"time":1640362108000},"attr":{"area":"大同市","uid":"478","app_v":"1.1.5","event_type":"common","device_id":"1de","os_type":"13.05","channel":"KA","language":"chinese","brand":"LG"}}
     ```
+  
+- OrderDB 类型
+
+  - 模拟 “电商平台” 订单数据库
+
+  - 目前仅实现了订单表，表结构如下
+
+  - ```mysql
+    CREATE TABLE IF NOT EXISTS orders(
+        `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, -- 记录id
+        `orderId` bigint(11) NOT NULL, --  订单id
+        `userId` bigint(11) NOT NULL, -- 用户id
+        `totalMoney` decimal(11,2) NOT NULL, -- 订单总金额（实付）
+        `areaId` int(11) NOT NULL, -- 区域最低一级id
+        `tradeSrc` tinyint(4) NOT NULL DEFAULT 0, -- 订单来源,0:网页商城,1:微信小程序,2:安卓App,3:苹果App
+        `payStatus` tinyint(4) NOT NULL DEFAULT 0, -- 支付状态,0:未支付;1:已支付，2:已退款
+        `orderLifeCycle` tinyint(4) NOT NULL DEFAULT 1, -- 订单生命周期：
+        -- 1:下单,2:下单后取消,4:超时取消,
+        -- 5:支付,6:支付后取消,
+        -- 7:发货,8:发货后取消,10:投递失败退回,12:拒收，
+        -- 13:收货,14:收货后取消,
+        -- 15:确认收货,16:退货,
+        -- 17:过了退货期(最终完成)
+        -- 偶数是取消或退货（对应下面的无效订单），17 是最终完成的订单，其他是执行中的订单
+        `orderStatus` tinyint(4) NOT NULL DEFAULT 0 ,
+        -- 订单状态，0:执行中（有效），1:完成（有效）,2:取消或退货（无效）
+        `createTime` varchar(25) NOT NULL, -- 下单时间
+        `payTime` varchar(25), -- 支付时间
+        `modifiedTime` varchar(25) DEFAULT '0000-00-00 00:00:00:000' NOT NULL -- 订单更新时间
+    );
+    ```
+
+  - 生成的是一个 SQLite 的数据库文件，可以直接使用 sqlite-tools 命令行工具直接导出为 csv 文件，比如：
+
+    - ```shell
+      sqlite3 -noheader -csv D:\test\order.db "select * from orders;" > data.csv
+      ```
+
+    - 然后再加载进 hive 表
+
 
 
 
@@ -48,7 +98,7 @@ gdflbd，generate datasets for learning big data，生成用于学习大数据�
 
 ### 测试效果及执行效率：
 
-- 检查生成的文件行数、大小：
+- AppStartLog 类型生成的文件行数、大小：
 
   - ```bash
     wc -lc ~/gdflbd0105/tiny/*
@@ -87,52 +137,66 @@ gdflbd，generate datasets for learning big data，生成用于学习大数据�
     
     ```
 
-- CentOS7，OpenJDK1.8 环境执行效率：
+- AppStartLog 类型的执行效率
+
+  - CentOS7，OpenJDK1.8 环境执行效率：
+
+    - ```log
+      一月 05, 2022 8:09:14 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>执行参数：[/root/gdflbd0105/tiny, AppStartLog, tiny, 2021-12-25, 14]
+      一月 05, 2022 8:09:14 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>总耗时：2,197 毫秒
+      ```
+
+    - ```log
+      一月 05, 2022 8:11:18 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>执行参数：[/root/gdflbd0105/small, AppStartLog, small, 2021-12-25, 14]
+      一月 05, 2022 8:11:18 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>总耗时：16,131 毫秒
+      ```
+
+    - ```log
+      一月 05, 2022 8:12:36 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>执行参数：[/root/gdflbd0105/medium, AppStartLog, medium, 2021-12-25, 14]
+      一月 05, 2022 8:12:36 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>总耗时：58,321 毫秒
+      ```
+
+    - ```log
+      一月 05, 2022 8:31:21 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>执行参数：[/root/gdflbd0105/large, AppStartLog, large, 2021-12-25, 14]
+      一月 05, 2022 8:31:21 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>总耗时：1,104,863 毫秒
+      ```
+
+  - Win10，OracleJDK1.8，IDEA 环境下执行效率：
+
+    - ```log
+      一月 05, 2022 8:38:28 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>执行参数：[D:\test\gdflbd\0105test\medium, AppStartLog, medium, 2021-12-25, 14]
+      一月 05, 2022 8:38:28 下午 monster.helloworld.gdflbd.Entry main
+      信息: >>>总耗时：75,773 毫秒
+      ```
+
+  - [参考](http://helloworld.monster/a_notice_of_using_java.util.logging.logger_class_in_multi_threaded/#%E8%83%8C%E6%99%AF)
+
+- OrderDB 类型的执行效率
 
   - ```log
-    一月 05, 2022 8:09:14 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>执行参数：[/root/gdflbd0105/tiny, AppStartLog, tiny, 2021-12-25, 14]
-    一月 05, 2022 8:09:14 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>总耗时：2,197 毫秒
+    信息: >>>程序运行报告：
+            执行参数：[D:\test\gdflbd\0212\orderdb_tiny, OrderDB, tiny, 2020-09-21, 8]
+            总耗时：65224 毫秒
     ```
-    
-  - ```log
-    一月 05, 2022 8:11:18 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>执行参数：[/root/gdflbd0105/small, AppStartLog, small, 2021-12-25, 14]
-    一月 05, 2022 8:11:18 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>总耗时：16,131 毫秒
-    ```
-
-  - ```log
-    一月 05, 2022 8:12:36 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>执行参数：[/root/gdflbd0105/medium, AppStartLog, medium, 2021-12-25, 14]
-    一月 05, 2022 8:12:36 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>总耗时：58,321 毫秒
-    ```
-
-  - ```log
-    一月 05, 2022 8:31:21 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>执行参数：[/root/gdflbd0105/large, AppStartLog, large, 2021-12-25, 14]
-    一月 05, 2022 8:31:21 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>总耗时：1,104,863 毫秒
-    ```
-
-  - huge 还没跑
-
-- Win10，OracleJDK1.8，IDEA 环境下执行效率：
-
-  - ```log
-    一月 05, 2022 8:38:28 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>执行参数：[D:\test\gdflbd\0105test\medium, AppStartLog, medium, 2021-12-25, 14]
-    一月 05, 2022 8:38:28 下午 monster.helloworld.gdflbd.Entry main
-    信息: >>>总耗时：75,773 毫秒
-    ```
-
 
 
 
 
 ## 日志
+
+- 2022-02-04
+  - 初步完成 OrderDB 类型
+  - 实现 orders（订单）表
+  - release 0.1.03
 
 - 2022-01-05
   - 使用多线程，大大提高执行效率
--- src/test/java/monster/hellworld/testClass.java
+++ src/test/java/monster/hellworld/testClass.java
@@ -1,112 +1,112 @@
-package monster.hellworld;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
-import java.sql.*;
-import java.text.DecimalFormat;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Random;
-
-
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import monster.helloworld.gdflbd.constants.GdflbdConstant;
-import monster.helloworld.gdflbd.domain.Order;
-import monster.helloworld.gdflbd.generator.OrderLifeCycleSimulator;
-import monster.helloworld.gdflbd.utils.DateTimeUtil;
-import monster.helloworld.gdflbd.utils.SqliteUtil;
-import org.junit.jupiter.api.Test;
-import org.sqlite.SQLiteConnection;
-
-public class testClass {
-
-    @Test
-    public void test6() {
-        System.out.println(Integer.MIN_VALUE);
-        System.out.println(Integer.MAX_VALUE);
-    }
-
-    @Test
-    public void test5() {
-        System.out.println(GdflbdConstant.FILE_NAME_PREFIX_JSON);
-        JSONObject jsonObject = JSON.parseObject(GdflbdConstant.FILE_NAME_PREFIX_JSON);
-        Map<String, Object> innerMap = jsonObject.getInnerMap();
-        System.out.println(innerMap);
-        System.out.println(innerMap.get(GdflbdConstant.DATA_TYPE[0]));
-
-
-//        String[] smartPhoneBrands = {
-//                "Alcatel", "Apple", "Honor", "Huawei", "LG", "Lenovo", "Motorola", "Nokia", "OnePlus", "Oppo",
-//                "Others", "Realme", "Samsung", "TECNO", "Vivo", "Xiaomi", "ZTE"
-//        };
-//        Arrays.sort(smartPhoneBrands);
-//        System.out.println(Arrays.toString(smartPhoneBrands));
-
-//        String[] cityArray = GdflbdConstant.CITY_ARRAY;
-//        System.out.println(cityArray.length);
-//        Arrays.sort(cityArray);
-//        System.out.println(Arrays.toString(cityArray));
-
-    }
-
-    @Test
-    public void testAppStartLog() {
-//        AppStartLogOneDayThread appStartLogOneDayThread = new AppStartLogOneDayThread(null);
+//package monster.hellworld;
 //
-//        System.out.println(appStartLogOneDayThread.getLogMessage(1595309484329L, 10000, true));
-
-
-//        AppStartLog appStartLog1 = new AppStartLog();
-//        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, true));
-//        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, true));
-//        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, false));
-//        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, false));
-
-
-//        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, true));
-//        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, true));
-//        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, false));
-//        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, false));
-    }
-
-
-    @Test
-    public void test3() {
-        Random random = new Random();
-        double randomAmount=Math.random()*10 + random.nextInt(100_000);
-        DecimalFormat decimalFormat = new DecimalFormat( "0.00" );
-        System.out.println(randomAmount);
-        System.out.println(decimalFormat.format(randomAmount));
-    }
-
-    @Test
-    public void test2() {
-        Order order = new Order();
-        order.setOrderId(1);
-        order.setUserId(1);
-        order.setTotalMoney(100.00F);
-        order.setAreaId(1);
-        order.setTradeSrc(0);
-        order.setPayStatus(0);
-        order.setOrderLifeCycle(1);
-        order.setOrderStatus(0);
-        order.setCreateTime("2022-02-02 14:28:000");
-        order.setPayTime("0000-00-00 00:00:000");
-        order.setModifiedTime("2022-02-02 14:28:000");
-        System.out.println(order);
-
-        do {
-            order = OrderLifeCycleSimulator.pushOnOrder(order, System.currentTimeMillis());
-            System.out.println(order);
-        } while (order.getOrderStatus() == 0);
-    }
-
-    @Test
-    public void test1() {
-        System.out.println(DateTimeUtil.timeStampToStr_1(1595309484329L));
-    }
-
-
-}
+//import java.lang.reflect.InvocationTargetException;
+//import java.lang.reflect.Method;
+//
+//import java.sql.*;
+//import java.text.DecimalFormat;
+//import java.util.Arrays;
+//import java.util.Map;
+//import java.util.Random;
+//
+//
+//import com.alibaba.fastjson.JSON;
+//import com.alibaba.fastjson.JSONObject;
+//import monster.helloworld.gdflbd.constants.GdflbdConstant;
+//import monster.helloworld.gdflbd.domain.Order;
+//import monster.helloworld.gdflbd.generator.OrderLifeCycleSimulator;
+//import monster.helloworld.gdflbd.utils.DateTimeUtil;
+//import monster.helloworld.gdflbd.utils.SqliteUtil;
+//import org.junit.jupiter.api.Test;
+//import org.sqlite.SQLiteConnection;
+//
+//public class testClass {
+//
+//    @Test
+//    public void test6() {
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE);
+//    }
+//
+//    @Test
+//    public void test5() {
+//        System.out.println(GdflbdConstant.FILE_NAME_PREFIX_JSON);
+//        JSONObject jsonObject = JSON.parseObject(GdflbdConstant.FILE_NAME_PREFIX_JSON);
+//        Map<String, Object> innerMap = jsonObject.getInnerMap();
+//        System.out.println(innerMap);
+//        System.out.println(innerMap.get(GdflbdConstant.DATA_TYPE[0]));
+//
+//
+////        String[] smartPhoneBrands = {
+////                "Alcatel", "Apple", "Honor", "Huawei", "LG", "Lenovo", "Motorola", "Nokia", "OnePlus", "Oppo",
+////                "Others", "Realme", "Samsung", "TECNO", "Vivo", "Xiaomi", "ZTE"
+////        };
+////        Arrays.sort(smartPhoneBrands);
+////        System.out.println(Arrays.toString(smartPhoneBrands));
+//
+////        String[] cityArray = GdflbdConstant.CITY_ARRAY;
+////        System.out.println(cityArray.length);
+////        Arrays.sort(cityArray);
+////        System.out.println(Arrays.toString(cityArray));
+//
+//    }
+//
+//    @Test
+//    public void testAppStartLog() {
+////        AppStartLogOneDayThread appStartLogOneDayThread = new AppStartLogOneDayThread(null);
+////
+////        System.out.println(appStartLogOneDayThread.getLogMessage(1595309484329L, 10000, true));
+//
+//
+////        AppStartLog appStartLog1 = new AppStartLog();
+////        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, true));
+////        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, true));
+////        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, false));
+////        System.out.println(appStartLog1.getLogMessage(1595309484329L, 10000, false));
+//
+//
+////        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, true));
+////        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, true));
+////        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, false));
+////        System.out.println(AppStartLog.getLogMessage(1595309484329L, 10000, false));
+//    }
+//
+//
+//    @Test
+//    public void test3() {
+//        Random random = new Random();
+//        double randomAmount=Math.random()*10 + random.nextInt(100_000);
+//        DecimalFormat decimalFormat = new DecimalFormat( "0.00" );
+//        System.out.println(randomAmount);
+//        System.out.println(decimalFormat.format(randomAmount));
+//    }
+//
+//    @Test
+//    public void test2() {
+//        Order order = new Order();
+//        order.setOrderId(1);
+//        order.setUserId(1);
+//        order.setTotalMoney(100.00F);
+//        order.setAreaId(1);
+//        order.setTradeSrc(0);
+//        order.setPayStatus(0);
+//        order.setOrderLifeCycle(1);
+//        order.setOrderStatus(0);
+//        order.setCreateTime("2022-02-02 14:28:000");
+//        order.setPayTime("0000-00-00 00:00:000");
+//        order.setModifiedTime("2022-02-02 14:28:000");
+//        System.out.println(order);
+//
+//        do {
+//            order = OrderLifeCycleSimulator.pushOnOrder(order, System.currentTimeMillis());
+//            System.out.println(order);
+//        } while (order.getOrderStatus() == 0);
+//    }
+//
+//    @Test
+//    public void test1() {
+//        System.out.println(DateTimeUtil.timeStampToStr_1(1595309484329L));
+//    }
+//
+//
+//}
